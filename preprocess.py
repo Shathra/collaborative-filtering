0d699bf4297975c863d23aaba11ef326825bf4f9
Shathra <selcuk.gulcan@bilkent.edu.tr>
2018-07-31 15:58:30 +0300

    faster scaling

--- preprocess.py
+++ preprocess.py
@@ -24,19 +24,41 @@ for user_id, group in df_by_user:
 
 scipy.sparse.save_npz("user_item_matrix", user_item_matrix)
 
-print("A")
+def scale_sparse_matrix_rows(s, lowval=0, highval=1):
+    d = s.data
+
+    lens = s.getnnz(axis=1)
+    idx = np.r_[0,lens[:-1].cumsum()]
+
+    maxs = np.maximum.reduceat(d, idx)
+    mins = np.minimum.reduceat(d, idx)
+
+    minsr = np.repeat(mins, lens)
+    maxsr = np.repeat(maxs, lens)
+
+    D = highval - lowval
+    scaled_01_vals = (d - minsr)/(maxsr - minsr)
+    d[:] = scaled_01_vals*D + lowval
+
+user_item_matrix_normalized = user_item_matrix
+scale_sparse_matrix_rows(user_item_matrix_normalized, -1, 1)
+scipy.sparse.save_npz("user_item_matrix_normalized", user_item_matrix_normalized)
+
+"""
 user_item_matrix = user_item_matrix.todense()
-print("B")
 # Normalize each row separate
 max_arr = user_item_matrix.max(axis=1)
 min_arr = np.where(user_item_matrix == 0, user_item_matrix.max(), user_item_matrix).min(axis=1)
-print("C")
 row_idx, col_idx = user_item_matrix.nonzero()
 user_item_matrix_normalized = scipy.sparse.csr_matrix((no_of_user, no_of_item))
-print("D")
 print(row_idx.shape)
+
+c = 0
 for row, col in zip(row_idx, col_idx):
 	element = user_item_matrix[row, col]
 	user_item_matrix_normalized[row, col] = 2 * ((element - min_arr[row]) / (max_arr[row] - min_arr[row])) - 1
-print("E")
+	c += 1
+	if c % 1000 == 0:
+		print(c)
 scipy.sparse.save_npz("user_item_matrix_normalized", user_item_matrix_normalized)
+"""
